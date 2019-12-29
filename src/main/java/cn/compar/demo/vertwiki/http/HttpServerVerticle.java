e87af5002cb56b0b58f4f0a1931acee163cc41bb
hongzy <hongzy@windows10.microdone.cn>
2019-12-29 21:36:05 +0800

    增加jwt代码和测试

--- src/main/java/cn/compar/demo/vertwiki/http/HttpServerVerticle.java
+++ src/main/java/cn/compar/demo/vertwiki/http/HttpServerVerticle.java
@@ -84,45 +84,38 @@ public class HttpServerVerticle extends AbstractVerticle {
 			context.clearUser();
 			context.response().setStatusCode(302).putHeader("Location", "/").end();
 		});
-		
-	
-		
+
 		templateEngine = FreeMarkerTemplateEngine.create(vertx);
 
 		Router apiRouter = Router.router(vertx);
 		JWTAuth jwtAuth = JWTAuth.create(vertx, new JWTAuthOptions()
-				  .setKeyStore(new KeyStoreOptions()
-				    .setPath("keystore.jceks")
-				    .setType("jceks")
-				    .setPassword("secret")));
+				.setKeyStore(new KeyStoreOptions().setPath("keystore.jceks").setType("jceks").setPassword("secret")));
 		apiRouter.route().handler(JWTAuthHandler.create(jwtAuth, "/api/token"));
 		apiRouter.get("/token").handler(context -> {
-		    JsonObject creds = new JsonObject()
-		        .put("username", context.request().getHeader("login"))
-		        .put("password", context.request().getHeader("password"));
-		    auth.authenticate(creds, authResult -> { 
-		        if (authResult.succeeded()) {
-		            User user = authResult.result(); 
-		            user.isAuthorized("create", canCreate -> { 
-		            user.isAuthorized("delete", canDelete -> {
-		                    user.isAuthorized("update", canUpdate -> {
-		                        String token = jwtAuth.generateToken( 
-		                            new JsonObject()
-		                            .put("username", context.request().getHeader("login"))
-		                            .put("canCreate", canCreate.succeeded() && canCreate.result())
-		                            .put("canDelete", canDelete.succeeded() && canDelete.result())
-		                            .put("canUpdate", canUpdate.succeeded() && canUpdate.result()),
-		                        new JWTOptions().setSubject("Wiki API").setIssuer("Vert.x"));
-		                        context.response().putHeader("Content-Type", "text/plain").end(token);
-		                    });
-		                }); 
-		            });
-		        }else{
-		            context.fail(401); 
-		        }
-		    }); 
+			JsonObject creds = new JsonObject().put("username", context.request().getHeader("login")).put("password",
+					context.request().getHeader("password"));
+			auth.authenticate(creds, authResult -> {
+				if (authResult.succeeded()) {
+					User user = authResult.result();
+					user.isAuthorized("create", canCreate -> {
+						user.isAuthorized("delete", canDelete -> {
+							user.isAuthorized("update", canUpdate -> {
+								String token = jwtAuth.generateToken(
+										new JsonObject().put("username", context.request().getHeader("login"))
+												.put("canCreate", canCreate.succeeded() && canCreate.result())
+												.put("canDelete", canDelete.succeeded() && canDelete.result())
+												.put("canUpdate", canUpdate.succeeded() && canUpdate.result()),
+										new JWTOptions().setSubject("Wiki API").setIssuer("Vert.x"));
+								context.response().putHeader("Content-Type", "text/plain").end(token);
+							});
+						});
+					});
+				} else {
+					context.fail(401);
+				}
+			});
 		});
-		
+
 		apiRouter.get("/pages").handler(this::apiRoot);
 		apiRouter.get("/pages/:id").handler(this::apiGetPage);
 		apiRouter.post().handler(BodyHandler.create());
@@ -190,22 +183,26 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void apiCreatePage(RoutingContext context) {
-		JsonObject page = context.getBodyAsJson();
-		if (!validateJsonPageDocument(context, page, "name", "markdown")) {
-			return;
-		}
-		dbService.createPage(page.getString("name"), page.getString("markdown"), reply -> {
-			if (reply.succeeded()) {
-				context.response().setStatusCode(201);
-				context.response().putHeader("Content-Type", "application/json");
-				context.response().end(new JsonObject().put("success", true).encode());
-			} else {
-				context.response().setStatusCode(500);
-				context.response().putHeader("Content-Type", "application/json");
-				context.response()
-						.end(new JsonObject().put("success", false).put("error", reply.cause().getMessage()).encode());
+		if (context.user().principal().getBoolean("canCreate", false)) {
+			JsonObject page = context.getBodyAsJson();
+			if (!validateJsonPageDocument(context, page, "name", "markdown")) {
+				return;
 			}
-		});
+			dbService.createPage(page.getString("name"), page.getString("markdown"), reply -> {
+				if (reply.succeeded()) {
+					context.response().setStatusCode(201);
+					context.response().putHeader("Content-Type", "application/json");
+					context.response().end(new JsonObject().put("success", true).encode());
+				} else {
+					context.response().setStatusCode(500);
+					context.response().putHeader("Content-Type", "application/json");
+					context.response().end(
+							new JsonObject().put("success", false).put("error", reply.cause().getMessage()).encode());
+				}
+			});
+		} else {
+			context.fail(401);
+		}
 	}
 
 	private boolean validateJsonPageDocument(RoutingContext context, JsonObject page, String... expectedKeys) {
@@ -221,14 +218,18 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void apiUpdatePage(RoutingContext context) {
-		int id = Integer.valueOf(context.request().getParam("id"));
-		JsonObject page = context.getBodyAsJson();
-		if (!validateJsonPageDocument(context, page, "markdown")) {
-			return;
+		if (context.user().principal().getBoolean("canUpdate", false)) {
+			int id = Integer.valueOf(context.request().getParam("id"));
+			JsonObject page = context.getBodyAsJson();
+			if (!validateJsonPageDocument(context, page, "markdown")) {
+				return;
+			}
+			dbService.savePage(id, page.getString("markdown"), reply -> {
+				handleSimpleDbReply(context, reply);
+			});
+		} else {
+			context.fail(401);
 		}
-		dbService.savePage(id, page.getString("markdown"), reply -> {
-			handleSimpleDbReply(context, reply);
-		});
 	}
 
 	private void handleSimpleDbReply(RoutingContext context, AsyncResult<Void> reply) {
@@ -245,10 +246,14 @@ public class HttpServerVerticle extends AbstractVerticle {
 	}
 
 	private void apiDeletePage(RoutingContext context) {
-		int id = Integer.valueOf(context.request().getParam("id"));
-		dbService.deletePage(id, reply -> {
-			handleSimpleDbReply(context, reply);
-		});
+		if (context.user().principal().getBoolean("canDelete", false)) {
+			int id = Integer.valueOf(context.request().getParam("id"));
+			dbService.deletePage(id, reply -> {
+				handleSimpleDbReply(context, reply);
+			});
+		} else {
+			context.fail(401);
+		}
 	}
 
 	private void indexHandler(RoutingContext context) {
--- src/test/java/cn/compar/demo/vertwiki/http/ApiTest.java
+++ src/test/java/cn/compar/demo/vertwiki/http/ApiTest.java
@@ -22,78 +22,83 @@ import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.ext.web.codec.BodyCodec;
 
 @RunWith(VertxUnitRunner.class)
-	public class ApiTest {
-		private Vertx vertx;
-		private WebClient webClient;
-		@Before
-		public void prepare(TestContext context) {
-			vertx = Vertx.vertx();
-			JsonObject dbConf = new JsonObject()
-				.put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_URL, 					"jdbc:hsqldb:mem:testdb;shutdown=true") 
-				.put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE, 4);
-			vertx.deployVerticle(new WikiDatabaseVerticle(),
-					new DeploymentOptions().setConfig(dbConf), context.asyncAssertSuccess());
-			vertx.deployVerticle(new HttpServerVerticle(), context.asyncAssertSuccess());
-			webClient = WebClient.create(vertx, new WebClientOptions()
-				.setDefaultHost("localhost")
-				.setDefaultPort(8080)
-				.setSsl(true)
-				.setTrustOptions(new JksOptions().setPath("server-keystore.jks").setPassword("secret")));
+public class ApiTest {
+	private Vertx vertx;
+	private WebClient webClient;
+	private String jwtTokenHeaderValue;
 
-		}
-		@After
-		public void finish(TestContext context) {
-			vertx.close(context.asyncAssertSuccess());
-		}
-		@Test
-		public void play_with_api(TestContext context) {
-			Async async = context.async();
-			JsonObject page = new JsonObject().put("name", "Sample").put(
-					"markdown", "# A page");
-			
-			Promise<HttpResponse<JsonObject>> postPagePromise = Promise.promise();
-			webClient.post("/api/pages")
-					.as(BodyCodec.jsonObject())
-					.sendJsonObject(page,postPagePromise);
-			
-			
-			Future<HttpResponse<JsonObject>> getPageFuture =	postPagePromise.future().compose(resp -> {
-				Promise<HttpResponse<JsonObject>> promise = Promise.promise();
-				webClient.get("/api/pages")
-					.as(BodyCodec.jsonObject())
+	@Before
+	public void prepare(TestContext context) {
+		vertx = Vertx.vertx();
+		JsonObject dbConf = new JsonObject()
+				.put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_URL, "jdbc:hsqldb:mem:testdb;shutdown=true")
+				.put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE, 4);
+		vertx.deployVerticle(new WikiDatabaseVerticle(), new DeploymentOptions().setConfig(dbConf),
+				context.asyncAssertSuccess());
+		vertx.deployVerticle(new HttpServerVerticle(), context.asyncAssertSuccess());
+		webClient = WebClient.create(vertx, new WebClientOptions().setDefaultHost("localhost").setDefaultPort(8080)
+				.setSsl(true).setTrustOptions(new JksOptions().setPath("server-keystore.jks").setPassword("secret")));
+
+	}
+
+	@After
+	public void finish(TestContext context) {
+		vertx.close(context.asyncAssertSuccess());
+	}
+
+	@Test
+	public void play_with_api(TestContext context) {
+		Async async = context.async();
+
+		Promise<HttpResponse<String>> tokenPromise = Promise.promise();
+		webClient.get("/api/token").putHeader("login", "foo") // <1>
+				.putHeader("password", "bar").as(BodyCodec.string()) // <2>
+				.send(tokenPromise);
+		Future<HttpResponse<String>> tokenFuture = tokenPromise.future(); // <3>
+
+		JsonObject page = new JsonObject().put("name", "Sample").put("markdown", "# A page");
+		Future<HttpResponse<JsonObject>> postPageFuture = tokenFuture.compose(tokenResponse -> {
+			Promise<HttpResponse<JsonObject>> promise = Promise.promise();
+			jwtTokenHeaderValue = "Bearer " + tokenResponse.body(); // <1>
+			webClient.post("/api/pages").putHeader("Authorization", jwtTokenHeaderValue) // <2>
+					.as(BodyCodec.jsonObject()).sendJsonObject(page, promise);
+			return promise.future();
+		});
+		Future<HttpResponse<JsonObject>> getPageFuture = postPageFuture.compose(resp -> {
+			Promise<HttpResponse<JsonObject>> promise = Promise.promise();
+			webClient.get("/api/pages").putHeader("Authorization", jwtTokenHeaderValue).as(BodyCodec.jsonObject())
 					.send(promise);
-				return promise.future();
-			});
-			
-			Future<HttpResponse<JsonObject>> updatePageFuture = 	getPageFuture.compose(	response -> {
-						JsonArray array = response.body().getJsonArray("pages");
-						context.assertEquals(1, array.size());
-						context.assertEquals(0, array.getJsonObject(0).getInteger("id"));
-						 Promise<HttpResponse<JsonObject>> promise = Promise.promise();
-						webClient.put("/api/pages/0")
-							.as(BodyCodec.jsonObject())
-							.sendJsonObject(new JsonObject().put("id", 0).put("markdown", "Oh Yeah!"),promise);
-						return promise.future();
-					});
-			
-			 Future<HttpResponse<JsonObject>> deletePageFuture = 	 updatePageFuture.compose(
-					response -> {
-						context.assertTrue(response.body().getBoolean("success"));
-						Promise<HttpResponse<JsonObject>> promise = Promise.promise();
-						webClient.delete("/api/pages/0")
-								.as(BodyCodec.jsonObject())
-								.send(promise);
-						return promise.future();
-					});
-			 
-			 deletePageFuture.setHandler(ar -> {
-			      if (ar.succeeded()) {
-			          context.assertTrue(ar.result().body().getBoolean("success"));
-			          async.complete();
-			        } else {
-			          context.fail(ar.cause());
-			        }
-			      });
-			 async.awaitSuccess(5000);
-		}
+			return promise.future();
+		});
+
+		Future<HttpResponse<JsonObject>> updatePageFuture = getPageFuture.compose(response -> {
+			JsonArray array = response.body().getJsonArray("pages");
+			context.assertEquals(1, array.size());
+			context.assertEquals(0, array.getJsonObject(0).getInteger("id"));
+			Promise<HttpResponse<JsonObject>> promise = Promise.promise();
+			JsonObject data = new JsonObject().put("id", 0).put("markdown", "Oh Yeah!");
+			webClient.put("/api/pages/0").putHeader("Authorization", jwtTokenHeaderValue).as(BodyCodec.jsonObject())
+					.sendJsonObject(data, promise);
+			return promise.future();
+		});
+
+		Future<HttpResponse<JsonObject>> deletePageFuture = updatePageFuture.compose(response -> {
+			context.assertTrue(response.body().getBoolean("success"));
+			Promise<HttpResponse<JsonObject>> promise = Promise.promise();
+			webClient.delete("/api/pages/0")
+			.putHeader("Authorization", jwtTokenHeaderValue)
+			.as(BodyCodec.jsonObject()).send(promise);
+			return promise.future();
+		});
+
+		deletePageFuture.setHandler(ar -> {
+			if (ar.succeeded()) {
+				context.assertTrue(ar.result().body().getBoolean("success"));
+				async.complete();
+			} else {
+				context.fail(ar.cause());
+			}
+		});
+		async.awaitSuccess(5000);
+	}
 }
